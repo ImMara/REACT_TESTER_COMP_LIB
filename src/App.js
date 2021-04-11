d9ba0b691366f1b0d0f85b0153b32be57c3868e7
Mara <alaneraerts@live.be>
2021-04-11 15:42:58 +0200

    trash

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Diagram from "./components/diagram/diagram";
 import RangeSlider from "./components/rangeSlider/rangeSlider";
-<<<<<<< HEAD
 import {useState, useEffect, useRef, useCallback} from "react";
-=======
-import {useState} from "react";
->>>>>>> origin/main
 
 function App(callback, deps) {
     const fakeValues = [
-        {name: "user 0", percent : 20 , color: "#00ff00", lock: false, max:100 },
-        {name: "user 1", percent : 20 , color: "#F34F41", lock: false, max:100 },
-        {name:"user 2", percent: 20 , color : "#77DD78", lock: false, max:100 },
-        {name:"user 3", percent: 20 , color: "#FBFA93", lock: false, max:100 },
-        {name:"user 4", percent: 20 , color:"#FEFFF3", lock: false, max:100 }
+        {name: "user 0", percent : 20 , color: "#00ff00", lock: true, max:80 },
+        {name: "user 1", percent : 20 , color: "#F34F41", lock: false, max:80 },
+        {name:"user 2", percent: 20 , color : "#77DD78", lock: false, max:80 },
+        {name:"user 3", percent: 20 , color: "#FBFA93", lock: false, max:80 },
+        {name:"user 4", percent: 20 , color:"#FEFFF3", lock: false, max:80 }
     ]
 
     const [values , setValues] = useState(fakeValues)
@@ -46,22 +42,12 @@ function App(callback, deps) {
                     v.percent = 0
                 }
             }
-<<<<<<< HEAD
             total2 += parseFloat(v.percent.toFixed(4))
         })
         const newValue = [...values]
         newValue[index].percent = Math.round(value)
         setValues(newValue)
     },[values])
-=======
-            total2+=parseFloat(v.percent)
-        })
-        console.log(total2)
-
-        values[index].percent = value;
-        setValues([...values])
-    }
->>>>>>> origin/main
 
   return (
     <div className="App">
